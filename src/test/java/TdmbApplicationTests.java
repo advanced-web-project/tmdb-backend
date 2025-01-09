927cd69e0a741b35e1b4ec01f10b93ce93cc04b6
21120455 <hoai0455fit@gmail.com>
2025-01-09 11:06:23 +0700

    fix: add some config ci/cd

---
+++ src/test/java/TdmbApplicationTests.java
@@ -0,0 +1,12 @@
+
+import com.movie.tdmb.TdmbApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TdmbApplication.class)
+class TdmbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
