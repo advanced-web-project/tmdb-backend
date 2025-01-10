74039be166c4daeed3057dbfb2a54bd60f7cbfba
21120455 <hoai0455fit@gmail.com>
2025-01-10 18:36:09 +0700

    fix: fix inject url CloudinaryUtil

--- src/main/java/com/movie/tdmb/util/CloudinaryUtil.java
+++ src/main/java/com/movie/tdmb/util/CloudinaryUtil.java
@@ -2,8 +2,6 @@ package com.movie.tdmb.util;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.NonFinal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,18 +9,15 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Map;
+
 @Component
-@RequiredArgsConstructor
 public class CloudinaryUtil {
     private final Cloudinary cloudinary;
-    @NonFinal
-    @Value("${cloudinary-url}")
-    public String CLOUDINARY_URL;
-    public CloudinaryUtil() {
-        // Automatically configured via CLOUDINARY_URL
-        this.cloudinary = new Cloudinary(CLOUDINARY_URL);  ;
-    }
     private final Logger logger = LoggerFactory.getLogger(CloudinaryUtil.class);
+    public CloudinaryUtil(@Value("${cloudinary-url}") String cloudinaryUrl) {
+        this.cloudinary = new Cloudinary(cloudinaryUrl);
+    }
+
     public String uploadImageToCloudinary(String imageUrl) {
         try {
             // Upload the image using its URL
