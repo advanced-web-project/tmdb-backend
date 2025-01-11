793fbdc7e2177ab1c32b09f1c598bc6248123932
21120455 <hoai0455fit@gmail.com>
2025-01-12 02:31:09 +0700

    fix: fix ci/cd test

--- src/test/java/com/movie/tdmb/TdmbApplicationTests.java
+++ src/test/java/com/movie/tdmb/TdmbApplicationTests.java
@@ -3,7 +3,7 @@ package com.movie.tdmb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TdmbApplication.class)
 class TdmbApplicationTests {
 
 	@Test
