912e673b0e1aef35871d2798afbc6cd1d3ae8d6a
21120455 <hoai0455fit@gmail.com>
2025-01-14 07:00:54 +0700

    feat: similar movie

--- src/main/java/com/movie/tdmb/model/UserHistory.java
+++ src/main/java/com/movie/tdmb/model/UserHistory.java
@@ -7,9 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Data
 @Document(collection = "user_history")
